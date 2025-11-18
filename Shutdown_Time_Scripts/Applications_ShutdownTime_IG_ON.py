02f78583a1138f55f606c385feb935d9e6b44264
Sowmya Ke <nani.bhoogapurapu1106@gmail.com>
2025-11-18 12:49:22 +0530

    Enhance: Update local_save_path to use timestamp from config for consistent report naming

--- Shutdown_Time_Scripts/Applications_ShutdownTime_IG_ON.py
+++ Shutdown_Time_Scripts/Applications_ShutdownTime_IG_ON.py
@@ -1052,7 +1052,7 @@ def start_shutdown_time_measurement(py_logger):
     cur_dt_time_obj = datetime.now()
     global local_save_path
     # local_save_path = Path(__file__).parents[1].joinpath("Reports", "04_Shutdown_Time", "20250707_12-34-56")
-    local_save_path = Path(__file__).parents[1].joinpath("Reports", "07_Shutdown_Time", cur_dt_time_obj.strftime("%Y%m%d_%H-%M-%S"))
+    local_save_path = Path(__file__).parents[1].joinpath("Reports", "07_Shutdown_Time", config.get('Current_Timestamp', cur_dt_time_obj.strftime("%Y%m%d_%H-%M-%S")))
     local_save_path.mkdir(parents=True, exist_ok=True)
     global current_timestamp
     # current_timestamp = '20250707_123456'
