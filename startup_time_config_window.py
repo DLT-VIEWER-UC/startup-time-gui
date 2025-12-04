6398f187dcd81282a8aaeafe3b2f3a13d1d99304
Sowmya Ke <nani.bhoogapurapu1106@gmail.com>
2025-12-04 21:13:51 +0530

    Fix: Unhandled python exception, button enablement logic in StartupTimeConfig dialog by removing unnecessary KPI check during validation

--- startup_time_config_window.py
+++ startup_time_config_window.py
@@ -1752,7 +1752,7 @@ class StartupTimeConfig(QDialog):
         for i in range(4):
             self.update_ecu_block_styles(self.ecu_block_list[i], self.ecu_error_list[i])
 
-        self.ok_btn.setEnabled(False if self.main_window.is_test_in_progress and self.main_window.is_KPI_selected else True)
+        self.ok_btn.setEnabled(False if self.main_window.is_test_in_progress else True)
         print(f"Validation result - ECU block list map: {self.ecu_block_list_selection_map}, ECU error list: {self.ecu_error_list}), is_partially_filled: {is_partially_filled}")
         return not is_partially_filled and any((
             not self.ecu_error_list[0] and self.ecu_block_list_selection_map[0],
